7b3ca821cd770fc11454d3715bb57cc24624abd3
Charlie DeTar <cfd@media.mit.edu>
2011-12-14 12:35:26 -0500

    removing duplicated get_long_url

--- tweets/management/commands/import_urls.py
+++ tweets/management/commands/import_urls.py
@@ -10,38 +10,6 @@ from tweets.utils import get_long_url
 
 logger = logging.getLogger(__name__)
 
-class HeadRequest(urllib2.Request):
-    def get_method(self):
-        return "HEAD"
-
-class RawRedirectHandler(urllib2.HTTPRedirectHandler):
-    def get_redirect(self, req, fp, code, msg, headers):
-        loc = headers['Location']
-        status = code
-        return loc, status
-    def http_error_301(self, *a, **k): return self.get_redirect(*a, **k)
-    def http_error_302(self, *a, **k): return self.get_redirect(*a, **k)
-    def http_error_307(self, *a, **k): return self.get_redirect(*a, **k)
-
-def get_long_url(short_url):
-    url = short_url
-    opener = urllib2.build_opener(RawRedirectHandler())
-    status = 0
-    while status != 200:
-        request = HeadRequest(url)
-        try:
-            url, status = opener.open(request, timeout=1)
-            url = force_unicode(url)
-            #print "..........  ", url
-        except (ValueError, urllib2.HTTPError, socket.timeout, ssl.SSLError, urllib2.URLError, httplib.BadStatusLine, httplib.InvalidURL):
-            break
-        except Exception as e:
-            logger.error(short_url)
-            logger.error(url)
-            logger.exception(e)
-            break
-    return url
-
 class Command(BaseCommand):
     args = 'requires two integers: the range of tweets to scan.'
     help = "Parse the URLs from all tweets"
