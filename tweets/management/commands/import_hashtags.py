81327c95a934d3b8724c92f199e630abfa4b0ee4
cfd <cfd@sunra.media.mit.edu>
2011-12-16 13:16:50 -0500

    hashtag import improvements

--- tweets/management/commands/import_hashtags.py
+++ tweets/management/commands/import_hashtags.py
@@ -10,8 +10,6 @@ from django.utils.encoding import force_unicode, DjangoUnicodeDecodeError
 
 from tweets.models import Tweet, HashTag
 
-cache = {}
-
 class Command(BaseCommand):
     args = ''
     help = "Import hashtags from all tweets."
@@ -19,17 +17,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         low = int(args[0])
         high = int(args[1])
-        chunk = 1000
+        chunk = 10000
 
-        for i in range(low, high, 1000):
-            print (i - low) / float(high - low)
+        for i in range(low, high, chunk):
+            print ((i - low) / float(high - low)) * 100
             for tweet in Tweet.objects.all()[i:i + chunk]:
                 results = re.finditer(r"(#\w+)", tweet.text)
                 for match in results:
                     hashtag = match.group(1).lower()
-                    if hashtag in cache:
-                        tag = cache[hashtag]
-                    else:
-                        tag, created = HashTag.objects.get_or_create(tag=hashtag)
-                        cache[hashtag] = tag
+                    tag, created = HashTag.objects.get_or_create(tag=hashtag)
                     tweet.tags.add(tag)
